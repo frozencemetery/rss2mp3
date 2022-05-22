424ab93fad8f9c4743488dfd094b9a06ac814df8
Robbie Harwood <rharwood@redhat.com>
2022-05-22 14:55:19 -0400

    Support updating feeds
    Signed-off-by: Robbie Harwood <rharwood@redhat.com>

--- main.c
+++ main.c
@@ -3,9 +3,12 @@
 /* For getline(). */
 #define _POSIX_C_SOURCE 200809L
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -14,9 +17,11 @@
 #include "feed.h"
 
 #define CONFIG_FILE ".podcasts"
+#define GUIDS_FILE ".guids"
 
 struct termios t;
 buf *config;
+buf *guids;
 
 static void cleanup(void) {
     char *buffer;
@@ -26,6 +31,9 @@ static void cleanup(void) {
     tcsetattr(0, TCSANOW, &t);
 
     buffer = destruct_buf(&config);
+    free(buffer);
+
+    buffer = destruct_buf(&guids);
     free(buffer);
 }
 
@@ -42,10 +50,11 @@ static void help(void) {
     printf("- h: help (this text)\n");
     printf("- a: add a feed\n");
     printf("- l: list feeds\n");
+    printf("- u: update feeds\n");
     printf("- q: quit\n");
 }
 
-static void load_config(void) {
+static void load_configs(void) {
     char *home;
 
     if (config) {
@@ -61,6 +70,27 @@ static void load_config(void) {
     }
 
     config = new_from_file(CONFIG_FILE);
+    guids = new_from_file(GUIDS_FILE);
+}
+
+static void sanitize(char *s) {
+    /* In my experience, the various Android filesystems and interfaces
+     * therein tend to be very bad at handling special characters.  Rather
+     * than risk creating a file that can't be deleted later, take the
+     * coward's way out.
+     *
+     * My apologies to anyone titling their episodes in a language that
+     * requires more of UTF-8.  Rather than create files with non-ascii
+     * characters in their names, though, I'd prefer patches to derive the
+     * filenames from the URL as well as the title. */
+
+    for (size_t i = 0; s[i]; i++) {
+        if ((s[i] >= '0' && s[i] <= '9') || (s[i] >= 'A' && s[i] <= 'Z') ||
+            (s[i] >= 'a' && s[i] <= 'z') || s[i] == '.' || s[i] == '_') {
+            continue;
+        }
+        s[i] = '_';
+    }
 }
 
 static void add_url(void) {
@@ -109,6 +139,154 @@ static void add_url(void) {
     unload_feed(fctx);
 }
 
+static bool seen_guid(char *guid) {
+    size_t cur_len, guid_len;
+    const char *cur;
+
+    guid_len = strlen(guid);
+
+    reset_cursor(guids);
+    while ((cur = yield_line(guids, &cur_len))) {
+        if (cur_len != guid_len) {
+            continue;
+        }
+
+        if (!memcmp(guid, cur, cur_len)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+static void record_guid(char *guid) {
+    reset_cursor(guids);
+    append_bytes(guids, guid, strlen(guid));
+    append_bytes(guids, "\n", 1);
+    flush_to_file(guids, GUIDS_FILE);
+}
+
+static void download_item(const char *feed_name, char *title, char *url) {
+    struct stat s;
+    int ret;
+    char *filename;
+    size_t filename_len;
+
+    ret = stat(feed_name, &s);
+    if (ret && errno == ENOENT) {
+        mkdir(feed_name, 0770);
+    } else if (ret) {
+        die("TODO stat %m\n");
+    }
+    ret = chdir(feed_name);
+    if (ret) {
+        die("TODO chdir %m\n");
+    }
+
+    sanitize(title);
+    filename_len = strlen(title) + strlen(".mp3");
+    filename = malloc(filename_len + 1);
+    if (!filename) {
+        die("TODO malloc %m\n");
+    }
+    memcpy(filename, title, strlen(title));
+    memcpy(filename + strlen(title), ".mp3", strlen(".mp3"));
+    filename[filename_len] = '\0';
+
+    dl_to_file(url, title);
+    free(filename);
+
+    ret = chdir("..");
+    if (ret) {
+        die("TODO chdir %m\n");
+    }
+}
+
+static void dl_help(void) {
+    printf("- h: help (this text)\n");
+    printf("- d: download item\n");
+    printf("- s: skip item this time\n");
+    printf("- f: finish with this feed\n");
+    printf("- m: mark as seen\n");
+    printf("- q: quit program entirely\n");
+}
+
+static void process_items(feed_context *fctx, const char *feed_name) {
+    char *title, *guid, *url, cmd;
+
+    while (next_item(fctx, &title, &guid, &url)) {
+        if (!seen_guid(guid)) {
+            printf("New item: %s\n", title);
+
+        prompt:
+            printf("{h for help} ");
+            fflush(stdout);
+            cmd = getchar();
+            printf("\n");
+
+            if (cmd == '\0' || cmd == 'q') {
+                printf("see you in the next one...\n");
+                exit(0);
+            } else if (cmd == 'h') {
+                dl_help();
+                goto prompt;
+            } else if (cmd == 'd') {
+                download_item(feed_name, title, url);
+                record_guid(guid);
+            } else if (cmd == 's') {
+                continue;
+            } else if (cmd == 'f') {
+                return;
+            } else if (cmd == 'm') {
+                record_guid(guid);
+            } else {
+                printf("unrecognized command: %c", cmd);
+                printf("valid commands:\n");
+                dl_help();
+                goto prompt;
+            }
+        }
+
+        feed_free(title);
+        feed_free(guid);
+        feed_free(url);
+    }
+}
+
+static void update_feeds(void) {
+    const char *line;
+    char *url, *title;
+    size_t line_len;
+    buf *feed_raw;
+    feed_context *fctx;
+
+    while ((line = yield_line(config, &line_len))) {
+        if (line[0] != 'u') {
+            continue;
+        }
+
+        url = strndup(line + strlen("u: "), line_len - strlen("u: "));
+        if (!url) {
+            die("TODO strndup %m\n");
+        }
+
+        feed_raw = dl_to_buf(url);
+        free(url);
+        if (!feed_raw) {
+            die("TODO dl error\n");
+        }
+
+        fctx = load_feed(feed_raw, &title);
+        if (!fctx) {
+            die("TODO load_feed error\n");
+        }
+        printf("Processing %s...\n", title);
+        sanitize(title);
+        process_items(fctx, title);
+        feed_free(title);
+    }
+    reset_cursor(config);
+}
+
 int main() {
     char cmd;
     const char *line;
@@ -126,8 +304,7 @@ int main() {
     }
 
     atexit(cleanup);
-
-    load_config();
+    load_configs();
 
     while (1) {
         printf("[h for help] ");
@@ -153,6 +330,8 @@ int main() {
                 }
             }
             reset_cursor(config);
+        } else if (cmd == 'u') {
+            update_feeds();
         } else {
             printf("command not recognized! Valid commands:\n");
             help();
